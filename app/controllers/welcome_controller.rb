4250c15949c63c7177c8a11f2b3b11bdef920be2
Robin Dittrich <robinodittrich@gmail.com>
2022-02-16 13:49:14 -0800

    Move user create action to user model.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,21 +5,4 @@ class WelcomeController < ApplicationController
 
   def register
   end
-
-  def create
-    user = User.create(user_params)
-    if user.save
-      redirect_to user_path(user.id)
-    else
-      flash[:alert] = "Error: Name can't be blank, Email can't be blank and must be valid."
-      redirect_to "/register"
-    end
-
-
-  end
-
-  private
-    def user_params
-      params.permit(:name, :email)
-    end
 end
